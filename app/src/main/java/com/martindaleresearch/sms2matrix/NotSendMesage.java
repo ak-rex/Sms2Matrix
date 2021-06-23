0336716c9bef688498a0ba937809ecaec4b28481
ak-rex <rex@linux.com>
2021-06-22 23:19:40 -0600

    Add files via upload

---
+++ app/src/main/java/com/martindaleresearch/sms2matrix/NotSendMesage.java
@@ -0,0 +1,29 @@
+package com.martindaleresearch.sms2matrix;
+
+/**
+ * Created by gerben on 9-10-17.
+ */
+
+class NotSendMesage {
+    private String type;
+    private String phone;
+    private String body;
+
+    public NotSendMesage(String phone, String body, String type) {
+        this.phone = phone;
+        this.body = body;
+        this.type = type;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
